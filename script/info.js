b1940a187b333ddacabc95a8ccf822d1c96e6c24
hugokrul <hugo.krul@gmail.com>
2023-03-15 20:01:44 +0100

    finish populating info.html

--- script/info.js
+++ script/info.js
@@ -68,10 +68,10 @@ const americaFerrera = new Actor("America Ferrera", 1984, ["The sisterhood", "Ba
 const jonahHill = new Actor("Jonah Hill", 1983, ["the wolf of wall street", "21 jump street", "How to train your dragon", "you people", "The lego Movie"], "../images/cast/jonahH.jpg", "Snotlout");
 const christofferMP = new Actor("Christoffer Mintz-Plasse", 1989, ["Superbad", "Kick-Ass 2", "how to train your dragon", "Role models", "Neighbors"], "../images/cast/christofferM.jpg", "Fishlegs");
 const tjMiller = new Actor("T.J. Miller", 1982, ["Office Christmas Party", "The Emoji Movie", "How to train your dragon", "Big Hero 6", "Underwater"], "../images/cast/tjM.jpg", "Tuffnut");
-const kristenWiig = new Actor("Kirsten Wiig", 1973, ["Bridesmaids", "Wonder Woman 1984", "How to train your dragon", "Te secret life of Walter Mitty", "Despicable me"], "../images/kirstenW.jpg", "Ruffnut");
-const robinAtkinDownes = new Actor("Robin Atkin Downes", 1976, ["King star king", "Superman vs. the elite", "How to train your dragon", "The werewolf reborn", "Meet the spy"], "../images/robinA.jpg", "Ack");
+const kristenWiig = new Actor("Kirsten Wiig", 1973, ["Bridesmaids", "Wonder Woman 1984", "How to train your dragon", "Te secret life of Walter Mitty", "Despicable me"], "../images/cast/kristenW.jpg", "Ruffnut");
+const robinAtkinDownes = new Actor("Robin Atkin Downes", 1976, ["King star king", "Superman vs. the elite", "How to train your dragon", "The werewolf reborn", "Meet the spy"], "../images/cast/robinA.jpg", "Ack");
 const kieronElliot = new Actor("Kieron Elliot", 1995, ["Solitary", "Night Eyes", "How to train your dragon", "Sir billi", "Episode 50"], "../images/cast/kieronE.jpg", "Hoark the Haggard");
-const ashleyJensen = new Actor("Ashley Jensen", 1969, ["Agatha Raisin", "Mayflies", "How to train your dragon", "Nativity!", "Christmas on Mistletoe Farm"], "../images/cast/ashelyJ.jpg", "Phlegma the Fierce");
+const ashleyJensen = new Actor("Ashley Jensen", 1969, ["Agatha Raisin", "Mayflies", "How to train your dragon", "Nativity!", "Christmas on Mistletoe Farm"], "../images/cast/ashleyJ.jpg", "Phlegma the Fierce");
 const davidTennant = new Actor("David Tennant", 1971, ["Doctor Who", "Des", "How to train your dragon", "The escape artist", "Gracepoint"], "../images/cast/davidT.jpg");
 
 let actors = [jayBaruchel, gerardButler, craigFerguson, americaFerrera, jonahHill, christofferMP, tjMiller, kristenWiig, robinAtkinDownes, kieronElliot, ashleyJensen];
@@ -82,7 +82,146 @@ const cressidaCowell = new Writer("Cressida Cowell", 1966, ["The Wizard of Once"
 // director 
 const deanDeBlois = new Director("Dean De Blois", 1970, ["How to train your dragon", "Lilo & Stitch", "Leroy & Stitch", "Helma"])
 
-const httyd = new Movie(2010, "How To Train Your Dragon", "Action/Fantasy", deanDeBlois, cressidaCowell, actors);
+const httyd = new Movie(2010, "How To Train Your Dragon", "Action/Fantasy", deanDeBlois, [cressidaCowell], actors);
+
+function populateHTML(movie) {
+	const movieSection = document.createElement('section');
+
+	const titleElement = document.createElement('h1');
+	titleElement.innerText = movie.title;
+	movieSection.appendChild(titleElement);
+
+	const yearAndGenreElement = document.createElement('h3');
+	yearAndGenreElement.innerText = movie.year + ' - ' + movie.genre;
+	movieSection.appendChild(yearAndGenreElement);
+
+	const directorElement = document.createElement('h4');
+	directorElement.innerText = 'Directed by: ' + movie.director.name;
+	movieSection.appendChild(directorElement);
+	
+	const writerElement = document.createElement('h4');
+	let writerText = 'Written by: '
+	for (let i = 0; i < movie.writers.length; i++ ) {
+		writerText += movie.writers[i].name
+		if (i == movie.writers.length - 1) {
+			break;
+		}
+		writerText += ', '
+	}
+	writerElement.innerText = writerText;
+	movieSection.appendChild(writerElement);
+	document.body.appendChild(movieSection);
+
+	const artistSection = document.createElement('section');
+	artistSection.style.display = "grid";
+	artistSection.style.gridTemplateColumns = "repeat(4, auto)";
+
+	const castH2 = document.createElement('h2');
+	castH2.innerText = "Cast:"
+	movieSection.appendChild(castH2);
+
+	for (let i = 0; i < movie.stars.length; i++) {
+		const starElement = document.createElement('div');
+		starElement.style.display = "flex";
+		starElement.style.flexDirection = "column";
+		starElement.style.alignItems = "center";
+		starElement.style.marginBottom = "20px";
+
+		const starImage = document.createElement('img');
+		starImage.setAttribute('src', movie.stars[i].picture);
+		starImage.setAttribute('width', '100px');
+		starImage.setAttribute('height', '135px');
+		
+		const starTextElement = document.createElement('div');
+		starTextElement.style.flexDirection = "column"
+		starTextElement.style.flexWrap = "wrap";
+		starTextElement.style.justifyContent = "center";
+		starTextElement.style.backgroundColor = "#3d3d3d";
+		starTextElement.style.padding = "10px";
+		starTextElement.style.borderRadius = "5px";
+		starTextElement.style.position = "absolute";
+		starTextElement.style.marginTop = "130px";
+		starTextElement.classList.add('hide');
+		
+		starImage.addEventListener('mouseover', () => {
+			starTextElement.classList.remove('hide');
+			starTextElement.classList.add('show');
+		})
+		starImage.addEventListener('mouseout', () => {
+			starTextElement.classList.add('hide');
+			starTextElement.classList.remove('show');
+		})
+		starTextElement.addEventListener('mouseover', () => {
+			starTextElement.classList.remove('hide');
+			starTextElement.classList.add('show');
+		})
+		starTextElement.addEventListener('mouseout', () => {
+			starTextElement.classList.add('hide');
+			starTextElement.classList.remove('show');
+		})
+		
+		const starTextActor = document.createElement('p');
+		starTextActor.style.textAlign = "left"
+		starTextActor.innerText = `${movie.stars[i].name} - ${movie.stars[i].yob} plays: ${movie.stars[i].character}`
+
+		const starTextMovies = document.createElement('p');
+		let starTextMoviesText = 'Plays in: '
+		const actorMovies = movie.stars[i].listOfMovies
+		for (let i = 0; i<actorMovies.length; i++) {
+			starTextMoviesText += actorMovies[i]
+			if (i == actorMovies.length - 1) {
+				break;
+			}
+			starTextMoviesText += ', '
+		}
+		starTextMovies.innerText = starTextMoviesText;
+
+		starTextMovies.style.color ="white";
+		starTextActor.style.color ="white";
+
+		starTextElement.appendChild(starTextActor);
+		starTextElement.appendChild(starTextMovies);
+
+		starElement.appendChild(starImage);
+		starElement.appendChild(starTextElement);
+		artistSection.appendChild(starElement)
+	}
+	document.body.appendChild(artistSection);
+
+	const posterTrailerPlotElement = document.createElement('section');
+
+	const posterH2 = document.createElement('h2');
+	posterH2.innerText = "Poster:"
+	posterTrailerPlotElement.appendChild(posterH2);
+
+	const posterElement = document.createElement('img');
+	posterElement.setAttribute('src', movie.poster);
+	posterElement.setAttribute('width', "200px");
+	posterElement.setAttribute('height', "270px");
+	posterTrailerPlotElement.appendChild(posterElement);
+
+	const TrailerH2 = document.createElement('h2');
+	TrailerH2.innerText = "Trailer:"
+	posterTrailerPlotElement.appendChild(TrailerH2);
+
+	const trailerElement = document.createElement('video');
+	trailerElement.setAttribute('controls', true)
+	const trailerSourceElement = document.createElement('source');
+	trailerSourceElement.setAttribute('src', movie.trailer);
+	trailerElement.appendChild(trailerSourceElement);
+	posterTrailerPlotElement.appendChild(trailerElement);
+
+	const plotH2 = document.createElement('h2');
+	plotH2.innerText = "Plot:"
+	posterTrailerPlotElement.appendChild(plotH2);
+
+	const plotElement = document.createElement('p');
+	plotElement.classList.add('plot')
+	plotElement.innerText = movie.plot;
+	posterTrailerPlotElement.appendChild(plotElement);
+
+	document.body.appendChild(posterTrailerPlotElement);
+}
 
 function refreshDropdowns() {
 	//get the dropdown;[p]
@@ -119,6 +258,8 @@ function refreshDropdowns() {
 		}
 	}
 }
+
+populateHTML(httyd);
 
 refreshDropdowns();
 
